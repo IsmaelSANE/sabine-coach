174746f3c5070cf3402c64bab7acc08a577bf5c1
Ismael Sane <six.echo.sa@gmail.com>
2020-06-12 12:58:00 +0200

    Make the website responsive

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 
 
 	$("#testimonial-slider").owlCarousel({
-	    paginationSpeed : 500,      
+	    paginationSpeed : 500,
 	    singleItem:true,
 	    autoPlay: 3000,
 	});
